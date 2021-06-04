ebaf31d9ad3349482a1f561d6a7b491a26990383
Joshua Hong <jjhong@ucsd.edu>
2021-06-03 17:09:40 -0700

    Change font class location

--- scripts/secondScript.js
+++ scripts/secondScript.js
@@ -55,10 +55,10 @@ client.on(client.Event.SDK_READY, function() {
 
   if (treatment === 'on') {
     // Insert code for on treatment
-    document.getElementsByTagName("main")[0].classList.add('different-font');
+    document.getElementsByTagName("body")[0].classList.add('different-font');
   } else if (treatment === 'off') {
     // Insert code for off treatment
-    document.getElementsByTagName("main")[0].classList.remove('different-font');
+    document.getElementsByTagName("body")[0].classList.remove('different-font');
   } else {
     // Insert code for control treatment
   }
